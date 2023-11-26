f509ca541a14754f1087083da804eb3bc549a4bc
Haley Morgan <haleymorgan3264@gmail.com>
2023-11-25 17:52:40 -0700

    add logic to upload data to s3 when running in prod/aws

--- go-motive-webhook/load.go
+++ go-motive-webhook/load.go
@@ -8,11 +8,17 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
+
+var S3_BUCKET string = "axle-motive-data"
 
 // this will be the load metadata that gets built and sent to subscribers
 
-func LoadMotiveData(event string, data []interface{}) (map[string]interface{}, error) {
+func LoadMotiveData(event string, data []interface{}) ([]byte, error) {
 	var loadInfo = make(map[string]interface{})
 	loadInfo["resource"] = event
 	loadInfo["data"] = data
@@ -24,23 +30,31 @@ func LoadMotiveData(event string, data []interface{}) (map[string]interface{}, e
 	// get number of items, add to load info response
 	loadInfo["num_items"] = len(data)
 
-	// TODO: load data into S3, persistant storage, etc.
-	return loadInfo, nil
+	// save data to s3 (skips if we're not running in prod)
+	key := "data/" + event
+	jsonData, s3Err := saveToS3(S3_BUCKET, key, loadInfo)
+	if s3Err != nil {
+		fmt.Println("Error:", s3Err)
+		return nil, s3Err
+	}
+
+	return jsonData, nil
 }
 
-func RunWebhook(url string, data map[string]interface{}) {
+func RunWebhook(url string, event string, jsonData []byte) error {
 
 	// convert final response to JSON payload
-	jsonData, err := json.Marshal(data)
-	if err != nil {
-		log.Println("Error:", err)
-		return
-	}
+	// jsonData, err := json.Marshal(data)
+	// if err != nil {
+	// 	log.Println("Error:", err)
+	// 	return err
+	// }
+
 	// submit post request to subscriber URL
 	response, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
 	if err != nil {
 		fmt.Println("Error:", err)
-		return
+		return err
 	}
 	defer response.Body.Close()
 
@@ -48,9 +62,53 @@ func RunWebhook(url string, data map[string]interface{}) {
 	responseData, err := io.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println("Error reading response body:", err)
-		return
+		return err
 	}
 
 	fmt.Println("Status Code:", response.Status)
 	fmt.Println("Response Body:", string(responseData))
+	return nil
+}
+
+func saveToS3(bucket string, s3Key string, data map[string]interface{}) ([]byte, error) {
+
+	// convert final response to JSON payload
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Println("Error:", err)
+		return nil, err
+	}
+
+	// only upload to s3 if we're not running in dev
+	if ENV == "PROD" {
+		// if we're running in prod, load data to s3
+		log.Println("Saving files to AWS S3")
+		// create aws session
+		s, err := session.NewSession(&aws.Config{
+			Region: aws.String("us-east-2"),
+		})
+		if err != nil {
+			log.Println("Error creating session in AWS, cannot store files to s3.")
+			log.Println(err)
+			return nil, err
+		}
+
+		// Create an S3 service client
+		s3Client := s3.New(s)
+
+		// Upload the JSON data to S3
+		_, err = s3Client.PutObject(&s3.PutObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(s3Key),
+			Body:   bytes.NewReader(jsonData),
+		})
+		if err != nil {
+			log.Println("Error storing files to s3.")
+			log.Println(err)
+			return nil, err
+		}
+
+		fmt.Printf("Map stored in S3 at s3://%s/%s\n", bucket, s3Key)
+	}
+	return jsonData, nil
 }
